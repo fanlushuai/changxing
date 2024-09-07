9955a14bc81bd45e4f6239c376279a96726c371f
auh <21078607+fanlushuai@users.noreply.github.com>
2024-09-07 22:41:41 +0800

    f

--- test.js
+++ test.js
@@ -1,212 +1,104 @@
-let hasGetCapturePremission = false;
+var intent = new Intent();
+//打开无障碍设置界面
+//intent.setAction("android.settings.ACCESSIBILITY_SETTINGS");
 
-function once(fn, context) {
-  var result;
-  // 注意，此处为了防止，并发，加了sync。
-  return sync(function () {
-    if (fn) {
-      log("执行一次");
-      result = fn.apply(context || this, arguments);
-      fn = null;
-    } else {
-      // log("已经执行")
-    }
-    return result;
-  });
-}
+//飞行模式
+//intent.setAction("android.settings.CAPTIONING_SETTINGS");
+//辅助功能
+//intent.setAction("android.settings.ACCESSIBILITY_SETTINGS");
+//添加账户
+//intent.setAction("android.settings.ADD_ACCOUNT_SETTINGS");
+//系统设置首页
+//intent.setAction("android.settings.AIRPLANE_MODE_SETTINGS");
+//APN设置
+//intent.setAction("android.settings.APN_SETTINGS");
+//应用管理
+//intent.setAction("android.settings.APPLICATION_SETTINGS");
+//节电助手
+//intent.setAction("android.settings.BATTERY_SAVER_SETTINGS");
+//蓝牙
+//intent.setAction("android.settings.BLUETOOTH_SETTINGS");
+//字幕
+//intent.setAction("android.settings.CAPTIONING_SETTINGS");
+//无线显示
+//intent.setAction("android.settings.CAST_SETTINGS");
+//移动网络
+//intent.setAction("android.settings.DATA_ROAMING_SETTINGS");
+//日期和时间设置
+//intent.setAction("android.settings.DATE_SETTINGS");
+//关于手机
+//intent.setAction("android.settings.DEVICE_INFO_SETTINGS");
+//显示设置
+//intent.setAction("android.settings.DISPLAY_SETTINGS");
+//互动屏保设置
+//intent.setAction("android.settings.DREAM_SETTINGS");
+//实体键盘
+//intent.setAction("android.settings.HARD_KEYBOARD_SETTINGS");
+//应用权限,默认应用设置,特殊权限
+//intent.setAction("android.settings.HOME_SETTINGS");
+//忽略电池优化设置
+//intent.setAction("android.settings.IGNORE_BATTERY_OPTIMIZATION_SETTINGS");
+//可用虚拟键盘设置
+//intent.setAction("android.settings.INPUT_METHOD_SETTINGS");
+//安卓键盘语言设置(AOSP)
+//intent.setAction("android.settings.INPUT_METHOD_SUBTYPE_SETTINGS");
+//内存和存储
+//intent.setAction("android.settings.INTERNAL_STORAGE_SETTINGS");
+//语言偏好设置
+//intent.setAction("android.settings.LOCALE_SETTINGS");
+//定位服务设置
+//intent.setAction("android.settings.LOCATION_SOURCE_SETTINGS");
+//所有应用
+//intent.setAction("android.settings.MANAGE_ALL_APPLICATIONS_SETTINGS");
+//应用管理
+//intent.setAction("android.settings.MANAGE_APPLICATIONS_SETTINGS");
+//与ACTION_HOME_SETTINGS相同
+//intent.setAction("android.settings.MANAGE_DEFAULT_APPS_SETTINGS");
+//在其他应用上层显示,悬浮窗
+//intent.setAction("android.settings.action.MANAGE_OVERLAY_PERMISSION");
+//安装未知应用 安卓8.0
+//intent.setAction("android.settings.MANAGE_UNKNOWN_APP_SOURCES");
+//可修改系统设置 权限
+intent.setAction("android.settings.action.MANAGE_WRITE_SETTINGS");
+//内存与存储
+//intent.setAction("android.settings.MEMORY_CARD_SETTINGS");
+//可用网络选择
+//intent.setAction("android.settings.NETWORK_OPERATOR_SETTINGS");
+//NFC设置
+//intent.setAction("android.settings.NFCSHARING_SETTINGS");
+//网络中的 更多设置
+//intent.setAction("android.settings.NFC_SETTINGS");
+//通知权限设置
+//intent.setAction("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
+//勿扰权限设置
+//intent.setAction("android.settings.NOTIFICATION_POLICY_ACCESS_SETTINGS");
+//打印服务设置
+//intent.setAction("android.settings.ACTION_PRINT_SETTINGS");
+//备份和重置
+//intent.setAction("android.settings.PRIVACY_SETTINGS");
+//安全设置
+//intent.setAction("android.settings.SECURITY_SETTINGS");
+//监管信息
+//intent.setAction("android.settings.SHOW_REGULATORY_INFO");
+//声音设置
+//intent.setAction("android.settings.SOUND_SETTINGS");
+//添加账户设置
+//intent.setAction("android.settings.SYNC_SETTINGS");
+//有权查看使用情况的应用
+//intent.setAction("android.settings.USAGE_ACCESS_SETTINGS");
+//个人词典
+//intent.setAction("android.settings.USER_DICTIONARY_SETTINGS");
+//辅助应用和语音输入
+//intent.setAction("android.settings.VOICE_INPUT_SETTINGS");
+//VPN设置
+//intent.setAction("android.settings.VPN_SETTINGS");
+//VR助手
+//intent.setAction("android.settings.VR_LISTENER_SETTINGS");
+//选择webview
+//intent.setAction("android.settings.WEBVIEW_SETTINGS");
+//高级WLAN设置
+//intent.setAction("android.settings.WIFI_IP_SETTINGS");
+//选择WIFI,连接WIFI
+//intent.setAction("android.settings.WIFI_SETTINGS");
 
-let OCR = {
-  autoPermisionScreenCapture: once(function () {
-    if (hasGetCapturePremission) {
-      log("当前有截图权限");
-      return;
-    }
-
-    console.log("自动申请截图权限");
-    let Thread = threads.start(function () {
-      if (auto.service == null) {
-        toast("无障碍未开启");
-        return;
-      }
-
-      let ele = textMatches(
-        /(.*录屏或投屏.*|.*录制或投射.*|允许|立即开始|统一)/
-      ).findOne(10 * 1000);
-
-      if (ele == null) {
-        // toast("未能发现截图权限弹窗")
-        return;
-      }
-      log("已经弹出权限确认界面");
-
-      let eles = textMatches(
-        /(.*录屏或投屏.*|.*录制或投射.*|允许|立即开始|统一)/
-      ).find();
-
-      if (eles.empty()) {
-        // toast("未能发现截图权限弹窗")
-        return;
-      }
-
-      let notMiui14Style = false;
-      for (let e of eles) {
-        let text = e.text();
-        if (
-          text.indexOf("立即开始") > 0 ||
-          text.indexOf("允许") > 0 ||
-          text.indexOf("统一")
-        ) {
-          notMiui14Style = true;
-          break;
-        }
-      }
-
-      if (notMiui14Style) {
-        log("可以找到立即开始");
-        let allowEle = textMatches(/(允许|立即开始|统一)/).findOne(10 * 1000);
-        if (allowEle) {
-          sleep(1500);
-          if (allowEle.clickable()) {
-            log("点击 元素");
-            let ok = allowEle.click();
-            return ok;
-          } else {
-            let b = allowEle.bounds();
-            log("按压 坐标");
-            return press(b.centerX(), b.centerY(), 1);
-          }
-        }
-      } else {
-        //  在miui 14中，立即开始，不可找到。使用推测的方式来处理
-        log("推测 立即开始 坐标");
-
-        let cancel = text("取消").findOne(10 * 10000);
-        if (cancel) {
-          log("取消 按钮 存在");
-          let x = device.width - cancel.bounds().centerX();
-          let y = cancel.bounds().centerY();
-
-          log("点击 推测坐标 %s %s", x, y);
-          sleep(500);
-          press(x, y, 1);
-        } else {
-          log("取消按钮不存在，推测失败");
-        }
-      }
-    });
-
-    log("申请权限");
-
-    //在一个会话中，调用两次申请截图权限。就会卡死。
-    if (!requestScreenCapture(false)) {
-      toast("请求截图权限失败");
-      return false;
-    } else {
-      Thread.interrupt();
-      log("已获得截图权限");
-      hasGetCapturePremission = true;
-      return true;
-    }
-  }),
-  getPostionByOCR: function (x, y, w, h) {
-    this.autoPermisionScreenCapture();
-    var img = captureScreen(); // 截取当前屏幕图像
-    var clip = images.clip(img, x, y, w, h); // 裁剪图像
-    let res = paddle.ocr(clip);
-    // [ OcrResult(confidence=0.93765306, preprocessTime=15.0, inferenceTime=465.0, text=20:35, bounds=Rect(85, 37 - 303, 85)),
-    //   OcrResult(confidence=0.696329, preprocessTime=15.0, inferenceTime=465.0, text=g4, bounds=Rect(739, 35 - 1020, 87))]
-    return res;
-  },
-};
-
-OCR.autoPermisionScreenCapture();
-
-// sleep(5 * 1000);
-
-function getData() {
-  let res = OCR.getPostionByOCR(0, 0, device.width, device.height);
-  log(res);
-
-  let dataPostion = 0;
-  let order = {};
-  let hasGetTime = false;
-  for (let r of res) {
-    let rText = r.text;
-    if (!hasGetTime) {
-      if (
-        rText.indexOf("今天") > -1 ||
-        rText.indexOf("明天") > -1 ||
-        rText.indexOf("后天") > -1
-      ) {
-        log("日期 %s", rText);
-        order.use_date = rText;
-        hasGetTime = true;
-      }
-      // 日期在第一位的。没有找到，日期，其他的都不用看了。
-      continue;
-    }
-
-    if (rText && rText.indexOf("收益（元）") > -1) {
-      dataPostion = 1;
-      continue;
-    }
-
-    if (dataPostion == 0) {
-      continue;
-    }
-
-    //有时候下一个是航班号。
-    if (dataPostion == 1 && rText.indexOf("航班号") > -1) {
-      log("包含航班号");
-      continue;
-    }
-
-    if (dataPostion == 1) {
-      log("里程 %s km", rText);
-      order.dis = rText;
-    } else if (dataPostion == 2) {
-      //   log("预计用时 %s", rText);
-      //   order.kmTime = rText;
-    } else if (dataPostion == 3) {
-      log("收益 %s 元", rText);
-      order.price = rText;
-    } else if (rText == "抢单") {
-      log("找到抢单按钮");
-      order.qiangBounds = r.bounds;
-      break;
-    }
-
-    dataPostion++;
-  }
-
-  if (order.price && order.km) {
-    log(order);
-    return order;
-  }
-
-  return null;
-}
-
-getData();
-
-// let yilian = {
-//   isAlert() {
-//     while (1) {
-//       sleep(500); //时间别太长，也别太短。太长费手机，太短速度达不到
-
-//       if (text("出车中...").findOnce()) {
-//         // 位于这个界面，才进行截图判断。
-//       } else {
-//         // 可能是弹出框了。
-//         // 如果出现，找不到任何元素的界面。就认为是弹出框了。
-//         let eles = visibleToUser(true)
-//           .boundsInside(0, 0, device.width - 1, device.height - 1)
-//           .find();
-//         if (eles == null) {
-//           log("可能弹出订单");
-//           log("截图分析");
-//         }
-//       }
-//     }
-//   },
-// };
+app.startActivity(intent);
